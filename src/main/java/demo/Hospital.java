82617561f38b5dd7bfc73c676d18e05691406ca9
hawaijar <hawaijar.js@gmail.com>
2022-05-26 23:34:31 +0530

    feat: Added bean-aware configuration

--- src/main/java/demo/Hospital.java
+++ src/main/java/demo/Hospital.java
@@ -1,12 +1,15 @@
 package demo;
 
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
 @Scope(scopeName = "prototype")
-public class Hospital {
+public class Hospital implements BeanNameAware {
 	@Value("Morning Sky Hospital")
 	private String name;
 
@@ -18,5 +21,14 @@ public class Hospital {
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	@Override
+	public void setBeanName(String name) {
+		System.out.println("Before bean initialisation");
+	}
+	@PostConstruct
+	public void postConstruct() {
+		System.out.println("After bean initialisation");
 	}
 }
