1603d59c5ea7a1cbe91fe7677b7cae453a4d778c
hawaijar <hawaijar.js@gmail.com>
2022-05-28 13:53:00 +0530

    feat: applied multiple Advices to a point (getter method(s))

--- src/main/java/demo/Logger.java
+++ src/main/java/demo/Logger.java
@@ -1,14 +1,21 @@
 package demo;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 
 @Aspect
 @Component
 public class Logger {
-	@Before("execution(public String demo.*.get*())")
-	public void log() {
-		System.out.println("Logging method called...");
+	@Before("allGetters()")
+	public void log1() {
+		System.out.println("log1 method called...");
 	}
+	@Before("allGetters()")
+	public void log2() {
+		System.out.println("log2 method called...");
+	}
+	@Pointcut("execution(public String demo.*.get*())")
+	public void allGetters(){}
 }
